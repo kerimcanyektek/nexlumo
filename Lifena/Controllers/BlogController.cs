1ddf8c6ddce3d2eeb6eb19a2dbc5e14a68f41323
Bahar Karabulut <107935727+baharkarabulut@users.noreply.github.com>
2024-09-30 23:14:24 +0300

    contact, blog, achievements pages have been completed.

---
+++ Lifena/Controllers/BlogController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lifena.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+		public ActionResult Blog1()
+		{
+			return View();
+		}
+
+		public ActionResult Blog2()
+		{
+			return View();
+		}
+
+		public ActionResult Blog3()
+		{
+			return View();
+		}
+
+		public ActionResult Blog4()
+		{
+			return View();
+		}
+
+		public ActionResult Blog5()
+		{
+			return View();
+		}
+	}
+}
